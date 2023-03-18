3b25b471fbce7ac50b4c25c5cccce57f05dbc26d
sukhendra <mpct5jan1998@gmail.com>
2023-03-18 14:08:50 +0530

    js added

---
+++ app.js
@@ -0,0 +1,12 @@
+const menuBtn = document.getElementById('menu-btn');
+const toggleMenu = document.getElementById('toggle-menu');
+const closeBtn = document.getElementById('close-btn');
+
+
+menuBtn.addEventListener('click', ()=> {
+    toggleMenu.style.display = "block";
+});
+
+closeBtn.addEventListener('click', ()=> {
+    toggleMenu.style.display = "none";
+});
